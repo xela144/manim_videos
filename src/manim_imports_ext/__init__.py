3e45436e93e6eeab7871f4a4b347c2391923f6d9
xela144 <alex.d.bardales@gmail.com>
2024-10-16 20:16:21 -0700

    Doing some janky python module stuff to get InteractiveScene

---
+++ src/manim_imports_ext/__init__.py
@@ -0,0 +1,15 @@
+from manimlib import *
+#from manimlib.mobject.svg.old_tex_mobject import *
+from manimlib.mobject.svg.tex_mobject import *
+
+from manim_imports_ext.custom.backdrops import *
+from manim_imports_ext.custom.banner import *
+from manim_imports_ext.custom.characters.pi_creature import *
+from manim_imports_ext.custom.characters.pi_creature_animations import *
+from manim_imports_ext.custom.characters.pi_creature_scene import *
+from manim_imports_ext.custom.deprecated import *
+from manim_imports_ext.custom.drawings import *
+from manim_imports_ext.custom.end_screen import *
+from manim_imports_ext.custom.filler import *
+from manim_imports_ext.custom.logo import *
+from manim_imports_ext.custom.opening_quote import *
